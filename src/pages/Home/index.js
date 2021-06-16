fbc1a1cde7b77f92b0e15f6efef662a4f7919e5f
Marcos Adriano <marcosadriano740@gmail.com>
2021-06-16 16:44:49 -0300

    Styles for login form finished

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 import './Home.css';
 
@@ -6,15 +7,19 @@ const Home = () => {
   return (
     <div className="home-container">
       <form className="login-form-container">
+        <h2>Enter with your account</h2>
         <div className="input-group-container">
           <label>Email</label>
-          <input type="email" name="email"/>
+          <input type="email" name="email" placeholder="Type your email" />
         </div>
         <div className="input-group-container">
           <label>Password</label>
-          <input type="password" name="password" />
+          <input type="password" name="password" placeholder="Type your password" />
         </div>
-        <button type="submit">Sign in</button>
+        <button type="submit" className="form-button">Sign in</button>
+        <Link to='/signup' className="form-link">
+          <span>Or Create a new account here</span>
+        </Link>
       </form>
     </div>
   );
