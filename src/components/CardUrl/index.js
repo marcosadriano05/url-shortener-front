e77115e4dcc62c7f1b2e152c05eb47682a3de6d7
Marcos Adriano <marcosadriano740@gmail.com>
2021-06-17 18:48:08 -0300

    Adding data to CardUrl component

--- src/components/CardUrl/index.js
+++ src/components/CardUrl/index.js
@@ -3,6 +3,22 @@ import React from 'react';
 import './CardUrl.css';
 
 const CardUrl = ({ original_url, shorted_url, created_at }) => {
+  const date = new Date(created_at);
+  const utc_offset = date.getTimezoneOffset();
+  date.setMinutes(date.getMinutes() - utc_offset);
+  
+  const dateString = `${
+    String(date.getDay()).padStart(2, '0')
+  }/${
+    String(date.getMonth()).padStart(2, '0')
+  }/${date.getUTCFullYear()} às ${
+    String(date.getUTCHours()).padStart(2, '0')
+  }:${
+    String(date.getUTCMinutes()).padStart(2, '0')
+  }:${
+    String(date.getUTCSeconds()).padStart(2, '0')
+  }`;
+
   return (
     <article>
       <span>URL original</span>
@@ -10,7 +26,7 @@ const CardUrl = ({ original_url, shorted_url, created_at }) => {
       <span>URL curta</span>
       <a href={ shorted_url } rel="noreferrer" target="_blank" className="form-link">{ shorted_url }</a>
       <span>Data de criação</span>
-      <p>Data</p>
+      <p>{ dateString }</p>
     </article>
   );
 };
