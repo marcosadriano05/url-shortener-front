512468b8d0ff062a53deeb824a9e4b129d906f2e
Marcos Adriano <marcosadriano740@gmail.com>
2021-06-17 13:33:06 -0300

    Fix: redirect error in dashboard

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,5 +1,5 @@
-import React, { useEffect } from 'react';
-import { Link, useHistory } from 'react-router-dom';
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
 import { useForm } from 'react-hook-form';
 
 import api from '../../services/api';
@@ -8,8 +8,7 @@ import './Dashboard.css';
 
 const Dashboard = () => {
   const { register, handleSubmit, formState: { errors } } = useForm();
-
-  const history = useHistory();
+  const [isAuth, setIsAuth] = useState(false);
 
   useEffect(() => {
     (async function isUserAuth () {
@@ -19,39 +18,49 @@ const Dashboard = () => {
           headers: { 'Authorization': `Bearer ${token}` }
         });
         
-        if (!response.data.isAuth) {
-          history.push('/');
+        if (response.data.isAuth) {
+          setIsAuth(true);
         }
       } catch (error) {
-        history.push('/');
+        localStorage.removeItem('token');
+        setIsAuth(false);
       }
     })();
-  }, [history]);
+  }, []);
 
   function handleUrlSubmit(data) {}
 
   return (
     <div className="dashboard-container">
-      <div className="dashboard-header">
-        <h1>Dashboard</h1>
-        <nav className="nav-header">
-          <button type="button" className="nav-button">Sair</button>
-        </nav>
-      </div>
-
-      <form className="dashboard-form-container" onSubmit={handleSubmit(handleUrlSubmit)}>
-        <h2>Gere uma URL curta</h2>
-
-        <div className="input-group-container">
-          <label>URL</label>
-          <input type="text" {...register("url", { required: true })} placeholder="Digite aqui sua URL" />
-          { (errors.url && errors.url.type === 'required') && ( 
-            <span className="alert-form-message">URL é obrigatória</span> 
-          ) }
+      { !isAuth ? (
+        <div className="noauth-message">
+          <span>Faça o login para poder acessara aplicação</span>
+          <Link to="/" className="form-link">Página de Login</Link>
         </div>
+      ) : (  
+        <>
+          <div className="dashboard-header">
+            <h1>Dashboard</h1>
+            <nav className="nav-header">
+              <button type="button" className="nav-button">Sair</button>
+            </nav>
+          </div>
 
-        <button type="submit" className="form-button">Gerar URL</button>
-      </form>
+          <form className="dashboard-form-container" onSubmit={handleSubmit(handleUrlSubmit)}>
+            <h2>Gere uma URL curta</h2>
+
+            <div className="input-group-container">
+              <label>URL</label>
+              <input type="text" {...register("url", { required: true })} placeholder="Digite aqui sua URL" />
+              { (errors.url && errors.url.type === 'required') && ( 
+                <span className="alert-form-message">URL é obrigatória</span> 
+              ) }
+            </div>
+
+            <button type="submit" className="form-button">Gerar URL</button>
+          </form>
+        </>
+      ) }
     </div>
   );
 };
